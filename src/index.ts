b585269bcd26fc0c410d957eace1820919dd2bed
Travis Zhang <tzhangm@trip.com>
2025-10-17 16:09:44 +0800

    chore: daily development

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,62 @@
-export const cli = () => {
-    console.log('cli');
-}
+import chalk from "chalk";
+import { program } from "commander";
+import { version } from "../package.json";
+// import { hitCommand } from "./commands/hit";
+import { mapCommand } from "./commands/upload.ts";
+
+const accent = chalk.greenBright;
+
+/**
+ * * Program Default Configuration
+ */
+const CLI_BEFORE_ALL_TXT = `canyon: The ${accent(
+    "Canyon",
+)} CLI - Version ${version} (${accent(
+    "https://github.com/canyon-project/canyon",
+)}) ${chalk.black.bold.bgYellowBright(" ALPHA ")} \n`;
+
+const CLI_AFTER_ALL_TXT = `\nFor more help, head on to ${accent(
+    "https://github.com/canyon-project/canyon",
+)}`;
+
+program
+    .name("canyon")
+    .version(
+        version,
+        "-v, --version",
+        "see the current version of canyon-uploader",
+    )
+    .usage("[options or commands] arguments")
+    .addHelpText("beforeAll", CLI_BEFORE_ALL_TXT)
+    .addHelpText("after", CLI_AFTER_ALL_TXT)
+    .configureHelp({
+        optionTerm: (option) => accent(option.flags),
+        subcommandTerm: (cmd) => accent(cmd.name(), cmd.usage()),
+        argumentTerm: (arg) => accent(arg.name()),
+    })
+    .showHelpAfterError(true);
+
+/**
+ * * CLI Commands with upload
+ */
+program
+    .command("upload")
+    .option("--dsn <dsn>", "dsn of the canyon server")
+    .allowExcessArguments(false)
+    .allowUnknownOption(false)
+    .description("modify react native project code to adapt to canyon")
+    .addHelpText(
+        "after",
+        `\nFor help, head on to ${accent(
+            "https://github.com/canyon-project/canyon",
+        )}`,
+    )
+    .action(async (params, options) => await mapCommand(params, options));
+
+export const cli = async (args: string[]) => {
+    try {
+        await program.parseAsync(args);
+    } catch (e) {
+        console.error(e);
+    }
+};
