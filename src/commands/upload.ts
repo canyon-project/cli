d02c8122d82996c8e69c9f3bbb43b4436a0bd7bd
Travis Zhang <wr.zhang25@gmail.com>
2025-10-23 19:01:15 +0800

    chore: daily development

--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -1,40 +1,40 @@
-import fs from "node:fs";
-import path from "node:path";
-import axios from "axios";
-import * as console from "node:console";
-export async function mapCommand(params:any, options:any) {
-    const { dsn, repo_id:repoID, commit_sha:sha,provider } = params;
-    if (!fs.existsSync(path.resolve(process.cwd(), ".canyon_output"))){
-        console.log('不存在')
-        return
-    }
-    const files = fs.readdirSync(path.resolve(process.cwd(), ".canyon_output"));
-    let data = {};
-    for (let i = 0; i < files.length; i++) {
-        const fileCoverageString = fs.readFileSync(
-            path.resolve(process.cwd(), ".canyon_output", files[i]),
-            "utf-8",
-        );
-        data = {
-            ...data,
-            ...JSON.parse(fileCoverageString),
-        };
-    }
+import * as console from 'node:console';
+import fs from 'node:fs';
+import path from 'node:path';
+import axios from 'axios';
+export async function mapCommand(params: any, options: any) {
+  const { dsn, repo_id: repoID, commit_sha: sha, provider } = params;
+  if (!fs.existsSync(path.resolve(process.cwd(), '.canyon_output'))) {
+    console.log('不存在');
+    return;
+  }
+  const files = fs.readdirSync(path.resolve(process.cwd(), '.canyon_output'));
+  let data = {};
+  for (let i = 0; i < files.length; i++) {
+    const fileCoverageString = fs.readFileSync(
+      path.resolve(process.cwd(), '.canyon_output', files[i]),
+      'utf-8',
+    );
+    data = {
+      ...data,
+      ...JSON.parse(fileCoverageString),
+    };
+  }
 
-    const p = {
-        dsn,
-        provider:provider||'gitlab',
-        repoID: repoID||process.env.CI_PROJECT_ID,
-        sha: sha||process.env.CI_COMMIT_SHA,
-        instrumentCwd: process.cwd(),
-        reportID: 'initial_coverage_data',
-        reportProvider: 'ci',
-        buildTarget:'',
-        coverage:Object.keys(data)
-    }
-    console.log(p)
-    await axios.post(dsn, {
-        ...p,
-        coverage: data,
-    });
+  const p = {
+    dsn,
+    provider: provider || 'gitlab',
+    repoID: repoID || process.env.CI_PROJECT_ID,
+    sha: sha || process.env.CI_COMMIT_SHA,
+    instrumentCwd: process.cwd(),
+    reportID: 'initial_coverage_data',
+    reportProvider: 'ci',
+    buildTarget: '',
+    coverage: Object.keys(data),
+  };
+  console.log(p);
+  await axios.post(dsn, {
+    ...p,
+    coverage: data,
+  });
 }
